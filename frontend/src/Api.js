75a24c34651e234bbc6bd8d53f4c65f6c51974da
Vyome Agarwal <vyome@ualberta.ca>
2023-01-07 17:35:22 -0700

    Sorry guys I added a bunch of stuff and forgot I did, so this commit message is useless, please read the code yourself! The code works thogit status!

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,28 +1,35 @@
 export function test() {
   console.log("hi from this test funtion");
+  return "hi from this test funtion";
 }
 
 export const v = async () => {
   const { Configuration, OpenAIApi } = require("openai");
 
   const configuration = new Configuration({
-    apiKey: '',
+    apiKey: "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",
   });
   console.log("Here is the response from the API:");
   const openai = new OpenAIApi(configuration);
-  
 
   const response = await openai.createCompletion({
-    model: "text-ada-001",
+    model: "text-davinci-003",
     prompt:
-      "Write a restaurant review based on these notes:\n\nName: The Blue Wharf\nLobster great, noisy, service polite, prices good.\n\nReview:",
+      "I have 2 members in my team. The programming languages I am most familiar with are: Java, C++. I have beginner years of experience with programming. Give me 4 project ideas for a 48hr hackathon",
     temperature: 0.5,
-    max_tokens: 64,
+    max_tokens: 2000,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   });
 
-  
-  console.log(" HI " + response.data.choices[0].text);
+  console.log(response)
+  console.log(response.data.choices[0].text);
+  return response.data.choices[0].text
+//   return "YOU GOT THIS";
+//   return response.data.choices[0].text;
 };
+
+
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { getCompletion, test, v } from './Api';
+import { v, test } from './Api';
+import React from 'react';
 
-function App() {
 
-  v()
 
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);    this.state = {response: "HELLO"};
+  }
+
+  componentDidMount() {
+    this.renderResponse();
+  }
+
+  renderResponse = async() => {
+    const res = await v();
+  
+    this.setState({response: res});
+  
+    console.log("Here is the response from the API:  " + res);
+  }
+
+  render() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +34,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           
+        </p>
+
+        <p>
+        {this.state.response}
         </p>
 
         <a
@@ -26,6 +51,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 
