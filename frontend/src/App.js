da2c3b4b2c40323c52431176794d5aabe55ae2b9
Sooraj-beep <soorajarakkal@gmail.com>
2023-01-07 17:14:47 -0700

    Added Prompt Generation helper function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import {GeneratePrompt} from './helper/GeneratePrompt';
 
 function App() {
+  const param = {
+    totalPeople: 2,
+    languages: "Java, C++",
+    experience: "2-4",
+    totalIdeas: 4,
+    duration: "48hr",
+    theme: "webapp"
+  }
+  GeneratePrompt(param);
+  
   return (
     <div className="App">
       <header className="App-header">
