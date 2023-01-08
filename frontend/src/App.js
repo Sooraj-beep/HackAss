835729209aa402b19c1a540a7028ba38dc7dc6a0
ashutoshlamba <55332461+ashutoshlamba@users.noreply.github.com>
2023-01-07 17:07:07 -0700

    added 2 fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Num_members from './NumberOfUsers';
+import Experience from './Experience';
 
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
       <header className="App-header">
       <Header/>
       </header>
+      <div className='Num-Users'>
+      <Num_members/>
+      </div>
+      <div className='Num-Users'>
+      <Experience/>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/NumberOfUsers.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function Num_members() {
+  const [numUsers, setnumUsers] = React.useState('');
+
+  const handleChange = (event) => {
+    setnumUsers(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Members</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={numUsers}
+          label="numUsers"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+          <MenuItem value={9}>9</MenuItem>
+        </Select>
+        <FormHelperText>number of members</FormHelperText>
+      </FormControl>
+    </div>
+  );
+}
