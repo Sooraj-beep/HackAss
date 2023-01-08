a9f3311a6ac2847d723d226097d904084ea3f0c2
Sooraj-beep <soorajarakkal@gmail.com>
2023-01-08 00:36:37 -0700

    pushing before friend so he deals with conflicts

--- frontend/src/DynamicList.js
+++ frontend/src/DynamicList.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
@@ -7,10 +8,11 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 //show the list of resources in a dialog popup
 function PopupDialog(props){
-    const { onClose, selectedValue, open } = props;
+    const {onClose, selectedValue, open } = props;
     const handleClose = () => {
         onClose(selectedValue);
       };
+
       return (
         <Dialog onClose={handleClose} open={open}>
           <DialogTitle>Resources</DialogTitle>
@@ -21,11 +23,19 @@ function PopupDialog(props){
       );
 }
 
+PopupDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
+
 export default function DynamicList(list2, {childToParent}) {
     console.log(list2);
     const [open, setOpen] = React.useState(false);
     const [selectedIndex, setSelectedIndex] = React.useState('');
+    const [selectedValue, setSelectedValue] = React.useState('');
     const handleListItemClick = (event, index) => {
+      setSelectedValue(list2.list2[index]);
       setSelectedIndex(index);
       list2.childToParent(index);
       setOpen(true);
@@ -44,7 +54,7 @@ export default function DynamicList(list2, {childToParent}) {
         onClick={(event) => handleListItemClick(event, i)}
         sx = {{textAlign: 'center'}}
         >
-        <PopupDialog selectedValue={item} open={open} onClose={handleClose} />
+        <PopupDialog selectedValue={selectedValue} open={open} onClose={handleClose} />
         <ListItemText primary = {item} />
       </ListItemButton>)}
         </List>
